393e1847769301ba0513126297e51f5e1e95828e
wenxqi <wenxuan.qi@capgemini.com>
2026-03-19 15:36:42 +0800

    4

--- src/main/java/com/example/interview/service/impl/DrawServiceImpl.java
+++ src/main/java/com/example/interview/service/impl/DrawServiceImpl.java
@@ -1,11 +1,21 @@
 package com.example.interview.service.impl;
+import com.example.interview.enums.QuestionLevel;
 import com.example.interview.service.DrawService;
+
+import com.example.interview.vo.resp.QuestionRespVO;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+
 
 @Service
 @AllArgsConstructor
 public class DrawServiceImpl implements DrawService {
 
 
+    @Override
+    public List<QuestionRespVO> draw(QuestionLevel level, int count, Integer allowDownLevels) {
+        return List.of();
+    }
 }
